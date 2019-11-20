8331f9337c9a3e856d6a72048a106127d70e413a
jantsaa <50743227+jantsaa@users.noreply.github.com>
2019-11-20 08:57:04 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,63 +1,81 @@
-/*const express = require("express");
-const mysql = require("mysql");
-const bodyParser = require("body-parser");
+//Initiallising node modules
+var express = require("express");
+var bodyParser = require("body-parser");
+var sql = require("mssql");
+var app = express(); 
 
+// Body Parser Middleware
+app.use(bodyParser.json()); 
 
-const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-//Create connection
-const db = mysql.createConnection({
-    host: 'kiinteistodata.database.windows.net',
-    user: 'Nepe',
-    password: 'K1vaK0ulu',
-    database: 'kiinteistodata',
-    port: '1433'
+//CORS Middleware
+app.use(function (req, res, next) {
+    //Enabling CORS 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
+    next();
 });
 
+//Setting up server
+ var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+ });
 
-db.connect((err) => {
-    if(err) {
-        throw err;
-    }
-    console.log("MYSQL Connected...");
+//Initiallising connection string
+var dbConfig = {
+    user:  "Nepe",
+    password: "K1vaK0ulu",
+    server: "kiinteistodata.database.windows.net",
+    database: "kiinteistodata"
+};
+
+//Function to connect to database and execute query
+var  executeQuery = function(res, query){             
+     sql.connect(dbConfig, function (err) {
+         if (err) {   
+                     console.log("Error while connecting database :- " + err);
+                     res.send(err);
+                  }
+                  else {
+                         // create Request object
+                         var request = new sql.Request();
+                         // query to the database
+                         request.query(query, function (err, res) {
+                           if (err) {
+                                      console.log("Error while querying database :- " + err);
+                                      res.send(err);
+                                     }
+                                     else {
+                                       res.send(res);
+                                            }
+                               });
+                       }
+      });           
+}
+
+//GET API
+app.get("/", function(req , res){
+                var query = "select * from [dbo.lampotila]";
+                executeQuery (res, query);
 });
 
-app.listen("5000", () => {
-    console.log("Server started");
-});*/
-
-const express = require("express");
-const bodyParser = require("body-parser");
-var mysql = require('mysql');
-
-const app = express();
-
-//Create connection
-const db = mysql.createConnection({
-    host: 'kiinteistodata.database.windows.net',
-    user: 'Nepe',
-    password: 'K1vaK0ulu',
-    database: 'kiinteistodata',
-    port: '1433'
+/*
+//POST API
+ app.post("/api/user", function(req , res){
+                var query = "INSERT INTO [user] (Name,Email,Password) VALUES (req.body.Name,req.body.Email,req.body.Password”);
+                executeQuery (res, query);
 });
 
-db.connect( (err) => {
-    if(err) {
-        throw err;
-    }
-    console.log("MYSQL Conneted....");
-})
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.get("/", (req, res) =>{
-    res.send("Testi");
-})
-
-app.listen("3000", () => {
-    console.log("Server started on port 3000");
+//PUT API
+ app.put("/api/user/:id", function(req , res){
+                var query = "UPDATE [user] SET Name= " + req.body.Name  +  " , Email=  " + req.body.Email + "  WHERE Id= " + req.params.id;
+                executeQuery (res, query);
 });
+
+// DELETE API
+ app.delete("/api/user /:id", function(req , res){
+                var query = "DELETE FROM [user] WHERE Id=" + req.params.id;
+                executeQuery (res, query);
+});
+*/
